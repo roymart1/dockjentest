cbb167c344d47c61b0ea8c4755c5d244cef44bf0
zentesty <4l1g4t0r90!>
2019-05-22 16:01:41 -0400

    Making run a small and displaying keyid

--- obja.py
+++ obja.py
@@ -1,5 +1,6 @@
 import argparse
 import time
+import os
 
 class ObjectA:
     def __init__(self):
@@ -10,14 +11,25 @@ class ObjectA:
         print("Call the method CallMe of ObjectA")
 
 
-if __name__ == '__main__':
-    for i in range(10000):
-        obja = ObjectA()
-        obja.callMe()
-        time.sleep(0.5)
 
-    parser = argparse.ArgumentParser(description='Short sample app')
-    parser.add_argument('-a', action="store_true", default=False)
-    parser.add_argument('-b', action="store", dest="b")
-    parser.add_argument('-c', action="store", dest="c", type=int)
-    parser.parse_args(['-a', '-bval', '-c', '3'])
+def reverse(s):
+  str = ""
+  for i in s:
+    str = i + str
+  return str
+
+if __name__ == '__main__':
+    var_keyID = os.environ.get('AWS_KEYID', 'NOTHING_NOTHING')
+    if var_keyID:
+        print(reverse(var_keyID))
+
+    # for i in range(10000):
+    #     obja = ObjectA()
+    #     obja.callMe()
+    #     time.sleep(0.5)
+    #
+    # parser = argparse.ArgumentParser(description='Short sample app')
+    # parser.add_argument('-a', action="store_true", default=False)
+    # parser.add_argument('-b', action="store", dest="b")
+    # parser.add_argument('-c', action="store", dest="c", type=int)
+    # parser.parse_args(['-a', '-bval', '-c', '3'])
